f682919e2953d653ba45df006d8f1f7f816b3868
CGX <2717630445@qq.com>
2025-08-25 04:40:21 +0800

    django实践——文章详情

--- python/djangotutorial/zhilblog/blog/views.py
+++ python/djangotutorial/zhilblog/blog/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,reverse
+from django.http.response import JsonResponse
+from django.urls.base import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from .models import Blog,BlogComment,BlogCategory
+from .forms import PubBlogForm
+from django.db.models import Q
+
 
 def index(request):
-    return render(request,'index.html')
+    blogs = Blog.objects.all()
+    return render(request, 'index.html', context={"blogs": blogs})
 
 def blog_detail(request,blog_id):
-    return render(request,'blog_detail.html')
+    try:
+        blog = Blog.objects.get(pk=blog_id)
+    except Exception as e:
+        blog = None
+    return render(request, 'blog_detail.html', context={'blog': blog})
 
+@require_http_methods(['GET', 'POST'])
+@login_required(login_url=reverse_lazy("blog_auth:login"))
 def pub_blog(request):
-    return render(request,'pub_blog.html')
+    if request.method == 'GET':
+        categories = BlogCategory.objects.all()
+        return render(request, 'pub_blog.html', context={"categories": categories})
+    else:
+        form = PubBlogForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            category_id = form.cleaned_data.get('category')
+            blog = Blog.objects.create(title=title, content=content, category_id=category_id, author=request.user)
+            return JsonResponse({"code": 200, "message": "博客发布成功！", "data": {"blog_id": blog.id}})
+        else:
+            print(form.errors)
+            return JsonResponse({'code': 400, "message": "参数错误！"})
--- python/djangotutorial/zhilblog/blogauth/urls.py
+++ python/djangotutorial/zhilblog/blogauth/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog_auth'
 
 urlpatterns = [
     path('login', views.blog_login, name='login'),
-    path('login', views.blog_logout, name='logout'),
+    path('logout', views.blog_logout, name='logout'),
     path('register', views.register, name='register'),
     path('email', views.send_email_captcha, name='email')
 ]
